dcd2eefb4a3ce07dd97f56a7923c2e83a8c95eac
Brian Bycynski <high5bri@hotmail.com>
2016-11-16 16:02:14 -0500

    Win/Lose logic complete, nothing on screen yet

--- public/main.js
+++ public/main.js
@@ -7,7 +7,52 @@ const handleButtonClick = (event) => {
   $('figure.player img').src = `/images/${player}.svg`
   $('figure.computer img').src = `/images/${computer}.svg`
 
-  // HINT: Check for win, lose or draw, then call `gameOver()` eventually.
+  console.log('player is', player)
+  console.log('computer is', computer)
+
+  if (player === 'rock') {
+    if (computer === 'scissors') {
+      console.log('PLAYER WINS')
+    }
+
+    if (computer === 'paper') {
+      console.log('COMPUTER WINS')
+    }
+
+    if (computer === 'rock') {
+      console.log('TIE')
+    }
+  }
+
+  if (player === 'paper') {
+    if (computer === 'rock') {
+      console.log('PLAYER WINS')
+    }
+
+    if (computer === 'scissors') {
+      console.log('COMPUTER WINS')
+    }
+
+    if (computer === 'paper') {
+      console.log('TIE')
+    }
+  }
+
+  if (player === 'scissors') {
+    if (computer === 'paper') {
+      console.log('PLAYER WINS')
+    }
+
+    if (computer === 'rock') {
+      console.log('COMPUTER WINS')
+    }
+
+    if (computer === 'scissors') {
+      console.log('TIE')
+    }
+  }
+
+    // HINT: Check for win, lose or draw, then call `gameOver()` eventually.
 }
 
 const getComputerMove = () => {
@@ -15,7 +60,7 @@ const getComputerMove = () => {
   return moves[Math.floor(Math.random() * moves.length)]
 }
 
-// HINT: Try calling `gameOver(true)` in the console.
+    // HINT: Try calling `gameOver(true)` in the console.
 const gameOver = (playerDidWin) => {
   if (playerDidWin) {
     $('.dialog h3').textContent = 'You won!'
@@ -26,7 +71,7 @@ const gameOver = (playerDidWin) => {
 }
 
 const resetGame = () => {
-  // TODO: Probably need to do more to reset the game here...
+        // TODO: Probably need to do more to reset the game here...
   $('figure.player img').src = '/images/unknown.svg'
   $('figure.computer img').src = '/images/unknown.svg'
   $('body').className = ''
